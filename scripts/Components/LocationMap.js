62f48aa9017b6df538f6d97ea4e14d7e19a80551
Akil Harris <akil.harris@gmail.com>
2015-12-19 15:21:36 -0500

    maps stuffs

--- scripts/Components/LocationMap.js
+++ scripts/Components/LocationMap.js
@@ -1,10 +1,49 @@
 import { default as React, Component } from "react";
-import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
+import {GoogleMapLoader, GoogleMap, Marker, Polyline, Polygon} from "react-google-maps";
 
 
 export default class LocationMap extends Component {
+  makeMarker() {
+    // onRightclick={this.handleMarkerRightclick.bind(this, index)}
+    return (
+      <Marker {...this.props.center} />
+    );
+  }
+
+  makePolygon() {
+    return (
+      <Polygon {...this.props.center} />
+    );
+  }
+
+  getChildren() {
+    switch(this.props.center.type) {
+      case "polygon":
+        return this.makePolygon();
+      case "marker":
+        return this.makeMarker();
+      // case "streetView":
+      //   return this.makePolygon();
+      default:
+        return "&nbsp;";
+    }
+  }
 
   render() {
-    return (<div></div>);
+    const children = this.getChildren();
+
+    return (
+      <GoogleMapLoader
+        containerElement={<div {...this.props} style={{ height: "100%" }} />}
+        googleMapElement={
+          <GoogleMap
+            ref={(map) => map && console.log(map.getZoom())}
+            defaultZoom={this.props.center.zoom}
+            defaultCenter={this.props.center.position}>
+            {children}
+          </GoogleMap>
+        }
+      />
+    );
   }
 }
