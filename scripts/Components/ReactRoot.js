29d73f738b4af6b5db82399f33530fc88285c368
Akil Harris <akil.harris@gmail.com>
2015-12-19 15:21:36 -0500

    maps stuffs

--- scripts/Components/ReactRoot.js
+++ scripts/Components/ReactRoot.js
@@ -3,6 +3,7 @@ function requireAll(r) { return r.keys().map(r); }
 import { default as React, Component } from "react";
 import Article from "./Article";
 import ArticleHeader from "./ArticleHeader";
+import LocationMap from "./LocationMap";
 import _ from "lodash";
 
 const galleryImages = requireAll(require.context('../../assets/', true, /.*/));
@@ -20,20 +21,42 @@ const imageData = [
 ];
 
 const mapLocations = [
+  { key: "exarcheia",
+    description: "Exarcheia Neighborhood",
+    zoom: 14,
+    path: [{lat: 37.98199, lng: 23.73399},
+           {lat: 37.98683, lng: 23.74368},
+           {lat: 37.99028, lng: 23.74090},
+           {lat: 37.99177, lng: 23.73137},
+           {lat: 37.98602, lng: 23.72987},
+           {lat: 37.98581, lng: 23.73073},
+           {lat: 37.98199, lng: 23.73399}],
+    options: { fillColor: "MidnightBlue",
+      fillOpacity: 0.25,
+      strokeColor: "MidnightBlue",
+      strokeOpacity: 0.65,
+      strokeWeight: 4 },
+    type: "polygon",
+    position: { lat: 37.9861256, lng: 23.7336206 }},
   { key: "panep00",
     description: "Panepistimiou and Voukourestiou Streets",
+    type: "marker",
+    zoom: 16,
     position: { lat: 37.977216, lng: 23.735637 }},
   { key: "panep01",
     description: "Panepistimiou and Benaki Streets",
+    type: "marker",
+    zoom: 16,
     position: { lat: 37.983186, lng: 23.730469 }},
-  { key: "exarcheia",
-    description: "Exarcheia Neighborhood",
-    position: { lat: 37.9861256, lng: 23.7336206 }},
   { key: "omonoia",
     description: "Omonoia Square",
+    type: "marker",
+    zoom: 16,
     position: { lat: 37.977216, lng: 23.725584 }},
   { key: "tzavella",
     description: "Tzavella St",
+    type: "streetview",
+    zoom: 16,
     position: { lat: 37.985331, lng: 23.734454 }},
 ];
 
@@ -98,22 +121,24 @@ class Media extends Component {
   }
 
   buildMap() {
-    return (<div></div>);
+    const center = this.props.locations[0];
+    return (
+      <div className="media-map" ref="mediaMap">
+        <LocationMap center={center} locations={this.props.locations} />
+      </div>);
   }
 
   render() {
     const { pctScroll } = this.props.measurements,
           idx = Math.floor(pctScroll * (this.props.images.length - 1)),
           headerClasses = (pctScroll > 0.1) ? "media-header article-header full-opacity" : "media-header article-header zero-opacity",
-          images = (this.props.images) ? this.buildImages(idx) : (&nbsp;),
-          map = (this.props.locations) ? this.buildMap() : (&nbsp;);
+          images = (this.props.images) ? this.buildImages(idx) : "&nbsp;",
+          mapElem = (this.props.locations) ? this.buildMap() : "&nbsp;";
 
     return (
       <div ref="media" className="media" style={{width: this.props.open ? "99vw" : "50vw"}}>
         <ArticleHeader headerClasses={headerClasses} />
-        {images}
-        {map}
-
+        {mapElem}
       </div>
     );
   }
@@ -176,11 +201,15 @@ export default class ReactRoot extends Component {
       pctScroll }};
   }
 
+  setMedia(href) {
+    console.log(href);
+  }
+
   render() {
     return (
       <div ref="root" className="react-root" style={{ height: this.state.measurements.contentHeight,
                                                       width: this.state.measurements.viewportWidth }}>
-        <Article ref="article" />
+        <Article ref="article" setMedia={this.setMedia.bind(this)} />
         <Media open={this.state.open} measurements={this.state.measurements} images={_.take(galleryImages,3)} locations={mapLocations} />
       </div>
     );
--- views/index.html.js
+++ views/index.html.js
@@ -7,12 +7,17 @@ import {
   WebpackStyleEntry,
 } from "reacthtmlpack/lib/entry";
 
+const googleMapsScript = process.env.GOOGLE_MAPS_KEY
+  ? `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places,visualization&key=${process.env.GOOGLE_MAPS_KEY}`
+  : 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places,visualization'
+
 export default (
   <html>
     <head>
-      <title>Hello World</title>
+      <title>Two Col Ideas</title>
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
       <meta charSet="UTF-8" />
+      <link rel="icon" href="http://www.vice.com/assets/images/vice/favicons/mstile-310x310.png"/>
       <WebpackStyleEntry
         chunkName="client"
         chunkFilepath="../scripts/client.js"
@@ -22,6 +27,7 @@ export default (
     </head>
     <body>
       <div id="reactContainer" />
+      <script type="text/javascript" src={googleMapsScript} />
       <WebpackScriptEntry
         chunkName="client"
         chunkFilepath="../scripts/client.js"
