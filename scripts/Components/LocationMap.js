52f527b3ab62bb63e9be2ad3a727012811718351
Akil Harris <akil.harris@gmail.com>
2015-12-19 10:59:11 -0500

    lets try a map

---
+++ scripts/Components/LocationMap.js
@@ -0,0 +1,10 @@
+import { default as React, Component } from "react";
+import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
+
+
+export default class LocationMap extends Component {
+
+  render() {
+    return (<div></div>);
+  }
+}
